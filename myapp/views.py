6f8e279819256993e9a8ca21a4db8304378e8ded
amina62 <amina.sohail62@gmail.com>
2021-07-29 12:33:25 +0100

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import FormView
+from .forms import JoinForm
+from .mixins import AjaxFormMixin
+
+
+class JoinFormView(AjaxFormMixin, FormView):
+    form_class = JoinForm
+    template_name = 'forms/ajax.html'
+    success_url = '/form-success/'
